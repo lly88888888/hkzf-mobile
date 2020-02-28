2cd25e45d87e1c96017a6735b9c293186d680ada
lly88888888 <1261939773@qq.com>
2020-02-28 17:50:53 +0800

    地图页渲染完成 部分功能未实现

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import Home from '../src/pages/Home'
 import CityList from './pages/CityList'
+import Map from './pages/Map'
 export default function App () {
   return (
     <Router>
-      <div>
+      <div className="app">
         {/* 重定向 */}
-        <Route path="/" render={() =><Redirect to="/home"/>}></Route>
+        <Route exact path="/" render={() =><Redirect to="/home"/>}></Route>
         <Route path="/home" component={Home}></Route>
-        <Route path="/citylist" component={CityList}></Route>
+        <Route exact path="/citylist" component={CityList}></Route>
+        <Route exact path="/map" component={Map}></Route>
       </div>
     </Router>
   )
